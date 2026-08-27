4775aee6ab6fd909b4871582446464c82eb31d43
Harry Alberto <h.cespedes@factureate.com>
2026-08-27 18:37:41 -0500

    update 27 1

--- WS/validapyme.php
+++ WS/validapyme.php
@@ -3,17 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-register_shutdown_function(function () {
-
-    $error = error_get_last();
-
-    if ($error !== NULL) {
-
-        print_r($error);
-
-    }
-
-});
+header('Content-Type: application/json; charset=utf-8');
 
 require("../conn/conn_db.inc");
 require("../conn/conn_db_param.inc");   //seguridad
@@ -61,34 +51,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         if ($v_provee_docugid != '') $v_provee_docugid .= ' y otro correo de nuestro proveedor de contratos digitales '.$varr_docudig['valorchar'].' con el contrato de vinculacion con FACTUREATE ';
         else $v_provee_docugid .= ' de nuestro proveedor de contratos digitales '.$varr_docudig['valorchar'].' con el contrato de vinculacion con FACTUREATE ';
       }
-      
-      $arr_mail = array('mail_salida' => 'pymes@factureate.com', 'nombre_salida' => 'Factureate',
-                        'mail_destino' => $input['email_repre'], 'subject' => 'Registro en FACTUREATE',
-                        'body' => 'Hola '.$input['nombre_repre'].',<br><br>Se ha registrado su empresa '.$input['nombre'].' con RNC '.$input['identificacion'].' en nuestra plataforma,
-                                debe seguir los siguientes pasos para terminar el registro:<br><ul>
-                                <li>Ingresar a nuestra plataforma con las credenciales al final de este correo</li>
-                                <li>Completar la informacion de su empresa y enviar su registro (botom enviar)</li>
-                                <li>Nuestros analistas en menos de 24 horas verificaran la documentacion y aprobaran su registro</li>
-                                <li>Recibira un correo '.$v_provee_docugid.', lo cual servira para poder realizar las operaciones de adelanto de facturas que usted solicite</li>
-                                <li>Recibira un correo de confirmacion, desde ese momento puede registrar sus facturas</li></ul><br>
+
+      //++++ prepracion de arreglo de respuesta
+      $output['mail_salida'] = 'pymes@factureate.com';
+      $output['nombre_salida'] = 'Factureate';
+      $output['mail_destino'] = $input['email_repre'];
+      $output['subject'] = 'Registro en FACTUREATE';
+      $output['body'] = 'Hola '.$input['nombre_repre'].',<br><br>
+                        Se ha registrado su empresa '.$input['nombre'].' con RNC '.$input['identificacion'].' en nuestra plataforma,
+                                debe seguir los siguientes pasos para completar el registro:<br><ul>
+                                <li>PASO 1: Ingresar a nuestra plataforma con las credenciales al final de este correo</li>
+                                <li>PASO 2: Completar la informacion de su empresa adjuntando en PDF el Registro Mercantil, Estatutos/Camara de Comercio donde aparezcan los accionistas,
+                                  Documento del Representante Legal</li>
+                                <li>PASO 3: Nuestros analistas en menos de 24 horas verificaran la documentacion y aprobaran su registro</li>
+                                <li>PASO 4: Recibira un correo '.$v_provee_docugid.', lo cual servira para poder realizar las operaciones de adelanto de facturas que usted solicite</li>
+                                <li>PASO 5: Recibira un correo de confirmacion, desde ese momento puede registrar sus facturas</li></ul><br>
                                 Sus credenciales de acceso son las siguientes:<br>
                                 <ul><li>usuario: '.$arr_resultado['identificacion'].'</li><li>contrase&ntilde;a: '.$arr_resultado['password'].'</li>
                                 <li>Link de la plataforma: <a href="'.$varr_link['valorchar'].'" target="_blank">Plataforma Factureate</a></li></ul><br><br>
                                 Gracias por confiar en nosotros, trabajaremos para conseguir el financiamiento que necesita.<br>
                                 * Tildes omitidas intencionalmente<br><br>
-                                <img src="cid:logo_factureate" width="100">',
-                        'firma' => '../img/logo.png',
-                        'firma_nombre' => 'logo_factureate');
-      $obj_mail->enviar_correo($arr_mail);
-
-      //@@@@ enviio de cooreo a los analistas de factureate
-      $arr_mail_perfil = array('perfilid' => 8, 'nombre_salida' => 'Operaciones FACTUREATE',
-                              'subject' => 'Registro de nuevo EMISOR', 
-                              'body' => 'Hola, <br><br>Se ha registrado a trav&eacute;s de la web el siguiente emisor;<br>
-                                      <ul><li>Nombre: '.$input['nombre'].'</li><li>RNC: '.$input['identificacion'].'</li></ul><br>
-                                      Gracias<br><br>
-                                      FACTUREATE');
-      $obj_mail->enviar_correo_xperfil($arr_mail_perfil);
+                                <img src="cid:logo_factureate" width="100">';
+      $output['firma'] = '../images/logo.png';
+      $output['firma_nombre'] = 'logo_factureate';
       
       //@@@@ ACTUALIZO LOS PATH DE LOS ARCHIVOS RECIBIDOS
       $v_carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/pdf/EMP_'.$input['nombre'].'_'.$input['identificacion'];
@@ -96,72 +81,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
       $v_carpeta_destino = $_SERVER['DOCUMENT_ROOT'].'/pdf/EMP_'.$input['nombre'].'_'.$input['identificacion'].'/vinculacion';
       if (!is_dir($v_carpeta_destino)) mkdir($v_carpeta_destino, 0777, true);
-
-      $v_carpeta_destino_db = '../pdf/EMP_'.$input['nombre'].'_'.$input['identificacion'].'/vinculacion';
-      $file_registro_mercantil = $v_carpeta_destino.'/certificado_existencia_'.$input['identificacion'].'.pdf';
-      $file_registro_mercantil_db = $v_carpeta_destino_db.'/certificado_existencia_'.$input['identificacion'].'.pdf';
-      $file_poderes = $v_carpeta_destino.'/ficha_rut_'.$input['identificacion'].'.pdf';
-      $file_poderes_db = $v_carpeta_destino_db.'/ficha_rut_'.$input['identificacion'].'.pdf';
-      $file_documento_repre = $v_carpeta_destino.'/documento_'.$input['nombre_repre'].'_'.$input['ap_repre'].'_'.$input['am_repre'].'.pdf';
-      $file_documento_repre_db = $v_carpeta_destino_db.'/documento_'.$input['nombre_repre'].'_'.$input['ap_repre'].'_'.$input['am_repre'].'.pdf';
-
-      $varr_path_documentos = array('registro_mercantil' => $file_poderes_db, 'documento_repre' => $file_documento_repre_db, 'poderes_empresa' => $file_registro_mercantil_db, 'empresa_id' => $resultado);
-
-      $obj_mae->registra_path_documentos_empresa($varr_path_documentos);
-      
-      //@@@@ TRANSFERENCIA DE ARCHIVOS
-      $host = "ftp.brdkairos.com";
-      $port = 21;
-      $user = "vincula_emisorwebco@factureate.com";
-      $password = "^_d[1SgwONoD+D{=";
-      //$arrpassword = explode(".",$password);
-      //$pass = $arrpassword[0];
-      $pass = $password;
-                
-      $connection = ftp_connect($host, $port);
-
-      if (!$connection) {
-        die("No conecta al FTP");
-      }
-
-      $login = ftp_login($connection, $user, $pass);
-
-      if (!$login) {
-          die("Login FTP incorrecto");
-      }
-
-      ftp_pasv($connection, true);
-
-      $varr_zona = $obj_mae->get_parametro_detalle(21);
-      date_default_timezone_set($varr_zona['valorchar']);
-      $v_hoy = date('Y-m-d');
-
-      //@@@@ DOCUMENTO IDENTIDAD
-      $destino = $file_documento_repre;
-      $origen = $v_hoy."_".$input['nombre']."_".$input['identificacion']."/documento_representante_".$input['nombre_repre']."_".$input['a_paterno_repre']."_".$input['a_materno_repre']."_".$input['nrodoc_repre'].".pdf";
-      $upload = ftp_get($connection, $destino, $origen, FTP_BINARY);
-      if (!$upload) { echo 'Fallo la subida al FTP'; }
-
-      //@@@@ REGISTRO MERCANTIL
-      $destino = $file_registro_mercantil;
-      $origen = $v_hoy."_".$input['nombre']."_".$input['identificacion']."/certificado_existencia_".$input['identificacion'].".pdf";
-      $upload = ftp_get($connection, $destino, $origen, FTP_BINARY);
-      if (!$upload) { echo 'Fallo la subida al FTP'; }
-
-      //@@@@ PODERES
-      $destino = $file_poderes;
-      $origen = $v_hoy."_".$input['nombre']."_".$input['identificacion']."/ficha_rut_".$input['identificacion'].".pdf";
-      $upload = ftp_get($connection, $destino, $origen, FTP_BINARY);
-      if (!$upload) { echo 'Fallo la subida al FTP'; }
-
-      ftp_close($connection);
-
-    } else $output = array('id' => 2, 'mensaje' => '');
+    } else $output = array('id' => 2, 'mensaje' => 'Fallo el registro de empresa');
   }
   
-  http_response_code(200);
+  echo json_encode($output);
+  exit();
 }
 //En caso de que ninguna de las opciones anteriores se haya ejecutado
-echo json_encode($output);
-exit;
+header("HTTP/1.1 400 Bad Request");
 ?>
